56f0263ef98ea3d6025e4e319b9a2981b7d9131f
dqnx19 <dan.k.19@outlook.com>
2026-06-23 18:17:25 +0200

    updated about page generating to tabs switching, removed email and location icon, edited favicon (removed bg), fixed index.html and readme.md

--- pages.js
+++ pages.js
@@ -1,4 +1,4 @@
-import { setAttribute, scrollUp, setTitle, setFavicon, setContentOfHeader, setContentOfMain, setContentOfFooter, importCSSFromList } from "https://js.nether.click/nether.js"
+import { setAttribute, scrollUp, setTitle, setFavicon, setContentOfHeader, setContentOfMain, setContentOfFooter, importCSSFromList, importJSFromList } from "https://js.nether.click/nether.js"
 
 setAttribute("html", "lang", "en")
 
@@ -19,20 +19,25 @@ importCSSFromList([
     "https://modern-web.nether.click/components/css/a.css",
     "https://modern-web.nether.click/components/css/all.css",
     "https://modern-web.nether.click/components/css/table.css",
+    "https://modern-web.nether.click/components/css/tabs-switching.css",
+])
+
+importJSFromList([
+    "https://modern-web.nether.click/components/js/tabs-switching.js",
 ])
 
 setContentOfHeader(`
-    <button onclick="showHome()" class="logo">
-        <img src="img/icons/favicon.png">
+    <button class="logo" onclick="showHome()" title="Displays Home Page">
+        <img src="img/icons/favicon.png" alt="dqnx dev logo">
     </button>
 `)
 
 setContentOfFooter(`
-    <button onclick="showHome()">
-        <img src="img/icons/favicon.png">
+    <button onclick="showHome()" title="Displays Home Page">
+        <img src="img/icons/favicon.png" alt="dqnx dev logo">
     </button>
-    <button onclick="showAbout()">
-        <img src="img/links-icons/about.svg">
+    <button onclick="showAbout()" title="Displays About Page">
+        <img src="img/links-icons/about.svg" alt="About page link icon">
     </button>
 `)
 
@@ -59,47 +64,57 @@ function showAbout() {
     setContentOfMain(`
         <h1>About</h1>
         <section>
-            <h2>Who i am</h2>
-            <p>I am a student interested in coding and technology. Here you’ll find a collection of my projects, along with details about my programming language skills.</p>
-        </section>
-        <section>
-            <h2>Contact</h2>
-            <p>Email: <a href="mailto:dqnx-dev@outlook.com">dqnx-dev@outlook.com</a></p>
-            <p>Location: Czech Republic</p>
-            <p>
-        </section>
-        <section>
-            <h2>Programming Languages Skills</h2>
-            <table>
-                <tr>
-                    <td>Language</td>
-                    <td>Level of skill</td>
-                </tr>
-                <tr>
-                    <td>HTML</td>
-                    <td>Intermediate</td>
-                </tr>
-                <tr>
-                    <td>CSS</td>
-                    <td>Intermediate</td>
-                </tr>
-                <tr>
-                    <td>Javascript</td>
-                    <td>Basic</td>
-                </tr>
-                <tr>
-                    <td>Python</td>
-                    <td>Basic</td>
-                </tr>
-                <tr>
-                    <td>PowerShell</td>
-                    <td>Basic</td>
-                </tr>
-                <tr>
-                    <td>Batch (windows cmd)</td>
-                    <td>Basics</td>
-                </tr>
-            </table>
+            <div class="tabs-switching">
+                <div class="tabs">
+                    <button class="tab active" onclick="showTab('who_i_am', this)">Who I am</button>
+                    <button class="tab" onclick="showTab('contact', this)">Contact</button>
+                    <button class="tab" onclick="showTab('programming_languages_skills', this)">Programming Languages Skills</button>
+                </div>
+
+                <div class="tab-content active" id="who_i_am">
+                    <h2>Who i am</h2>
+                    <p>I am a student interested in coding and technology. Here you’ll find a collection of my projects,
+                        along with details about my programming language skills.</p>
+                </div>
+                <div class="tab-content" id="contact">
+                    <h2>Contact</h2>
+                    <p>Email: <a href="mailto:dqnx-dev@outlook.com">dqnx-dev@outlook.com</a></p>
+                    <p>Github: <a href="https://github.com/dqnx19">https://github.com/dqnx19</a></p>
+                </div>
+                <div class="tab-content" id="programming_languages_skills">
+                    <h2>Programming Languages Skills</h2>
+                    <table>
+                        <tr>
+                            <td>Language</td>
+                            <td>Level of skill</td>
+                        </tr>
+                        <tr>
+                            <td>HTML</td>
+                            <td>Intermediate</td>
+                        </tr>
+                        <tr>
+                            <td>CSS</td>
+                            <td>Intermediate</td>
+                        </tr>
+                        <tr>
+                            <td>Javascript</td>
+                            <td>Basic</td>
+                        </tr>
+                        <tr>
+                            <td>Python</td>
+                            <td>Basic</td>
+                        </tr>
+                        <tr>
+                            <td>PowerShell</td>
+                            <td>Basic</td>
+                        </tr>
+                        <tr>
+                            <td>Batch (windows cmd)</td>
+                            <td>Basics</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </section>
     `)
 }
