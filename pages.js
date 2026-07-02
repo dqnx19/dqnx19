da19b131c88773c7fbca8b3387257e03f5da169b
dqnx19 <dan.k.19@outlook.com>
2026-07-02 09:27:45 +0200

    changed url to ui components

--- pages.js
+++ pages.js
@@ -5,25 +5,25 @@ setAttribute("html", "lang", "en")
 setFavicon("img/icons/favicon.png")
 
 importCSSFromList([
-    "https://modern-web.nether.click/components/css/logo.css",
+    "https://web-ui.nether.click/components/css/logo.css",
     "https://modern-web.nether.click/fonts/lexend/lexend.css",
-    "https://modern-web.nether.click/components/css/body.css",
-    "https://modern-web.nether.click/components/css/header.css",
-    "https://modern-web.nether.click/components/css/headings.css",
-    "https://modern-web.nether.click/components/css/main.css",
-    "https://modern-web.nether.click/components/css/header.css",
-    "https://modern-web.nether.click/components/css/footer.css",
-    "https://modern-web.nether.click/components/css/grouped-list.css",
-    "https://modern-web.nether.click/components/css/section.css",
-    "https://modern-web.nether.click/components/css/p.css",
-    "https://modern-web.nether.click/components/css/a.css",
-    "https://modern-web.nether.click/components/css/all.css",
-    "https://modern-web.nether.click/components/css/table.css",
-    "https://modern-web.nether.click/components/css/tabs-switching.css",
+    "https://web-ui.nether.click/components/css/body.css",
+    "https://web-ui.nether.click/components/css/header.css",
+    "https://web-ui.nether.click/components/css/headings.css",
+    "https://web-ui.nether.click/components/css/main.css",
+    "https://web-ui.nether.click/components/css/header.css",
+    "https://web-ui.nether.click/components/css/footer.css",
+    "https://web-ui.nether.click/components/css/grouped-list.css",
+    "https://web-ui.nether.click/components/css/section.css",
+    "https://web-ui.nether.click/components/css/p.css",
+    "https://web-ui.nether.click/components/css/a.css",
+    "https://web-ui.nether.click/components/css/all.css",
+    "https://web-ui.nether.click/components/css/table.css",
+    "https://web-ui.nether.click/components/css/tabs-switching.css",
 ])
 
 importJSFromList([
-    "https://modern-web.nether.click/components/js/tabs-switching.js",
+    "https://web-ui.nether.click/components/js/tabs-switching.js",
 ])
 
 setContentOfHeader(`
